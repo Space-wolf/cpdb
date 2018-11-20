04b755365b51c6366fa905128a3c0e84e4b42177
Space-wolf <952867436@qq.com>
2018-11-20 10:12:30 +0800

    准备服务器环境调试

--- js/server.js
+++ js/server.js
@@ -1,19 +1,20 @@
 $(function(){
     //请求域名
-    var baseURL = "http://gbrssj.hylanda.com/";
+    var baseURL = "https://www.easy-mock.com/mock/5bf36194cc4d7e6060d04c51";
 
     $('#ret_btn').click(function() { 
         var s_value = $('#p_type').val();
         var tValue = $('#text_value').val();
+        var sendData = {"type":type, "keyword": tValue};
         var type = 1;
         if(s_value == "姓名"){
             type = 1;
         }else if(s_value == "职位"){
             type = 2;
         }
-        //data:JSON.stringify(sendData),//使用变量sendData
+        //data:data: JSON.stringify(sendData),//使用变量sendData
         $.ajax({
-            type: "post",
+            type: "get",
             url: baseURL+"/Home/Search/searchdetail",
             data: "type="+type+"&keyword="+encodeURI(tValue),
             contentType: "application/json",
